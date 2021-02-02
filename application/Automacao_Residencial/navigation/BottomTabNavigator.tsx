ff12f18ffcb502fce1908f626175f00d99396764
Miguel Eto Sobral Teixeira <mest2011mest@gmail.com>
2021-02-02 19:05:32 -0300

    Update git ignore and rename app project path

---
+++ application/Automacao_Residencial/navigation/BottomTabNavigator.tsx
@@ -0,0 +1,74 @@
+import { Ionicons } from '@expo/vector-icons';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createStackNavigator } from '@react-navigation/stack';
+import * as React from 'react';
+
+import Colors from '../constants/Colors';
+import useColorScheme from '../hooks/useColorScheme';
+import TabOneScreen from '../screens/TabOneScreen';
+import TabTwoScreen from '../screens/TabTwoScreen';
+import { BottomTabParamList, TabOneParamList, TabTwoParamList } from '../types';
+
+const BottomTab = createBottomTabNavigator<BottomTabParamList>();
+
+export default function BottomTabNavigator() {
+  const colorScheme = useColorScheme();
+
+  return (
+    <BottomTab.Navigator
+      initialRouteName="TabOne"
+      tabBarOptions={{ activeTintColor: Colors[colorScheme].tint, style:{backgroundColor: '#5c80bc'}, showLabel: false}}>
+      <BottomTab.Screen
+        name="Definições"
+        component={TabOneNavigator}
+        options={{
+          tabBarIcon: ({ color }) => <TabBarIcon name="ios-options-outline" color={"#E8C547"} />,
+        }}
+      />
+      <BottomTab.Screen
+        name="configurações"
+        component={TabTwoNavigator}
+        options={{
+          tabBarIcon: ({ color }) => <TabBarIcon name="ios-cog-outline" color={"#E8C547"} />,
+        }}
+      />
+    </BottomTab.Navigator>
+  );
+}
+
+// You can explore the built-in icon families and icons on the web at:
+// https://icons.expo.fyi/
+function TabBarIcon(props: { name: string; color: string }) {
+  return <Ionicons size={30} style={{ marginBottom: -3 }} {...props} />;
+}
+
+// Each tab has its own navigation stack, you can read more about this pattern here:
+// https://reactnavigation.org/docs/tab-based-navigation#a-stack-navigator-for-each-tab
+const TabOneStack = createStackNavigator<TabOneParamList>();
+
+function TabOneNavigator() {
+  return (
+    <TabOneStack.Navigator>
+      <TabOneStack.Screen
+        name="TabOneScreen"
+        component={TabOneScreen}
+        options={{ headerShown: false, headerTitle: 'Definições' }}
+        
+      />
+    </TabOneStack.Navigator>
+  );
+}
+
+const TabTwoStack = createStackNavigator<TabTwoParamList>();
+
+function TabTwoNavigator() {
+  return (
+    <TabTwoStack.Navigator>
+      <TabTwoStack.Screen
+        name="TabTwoScreen"
+        component={TabTwoScreen}
+        options={{ headerTitle: 'Configurações' }}
+      />
+    </TabTwoStack.Navigator>
+  );
+}
---
+++ application/Automacao_Residencial/screens/TabTwoScreen.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+import { useState } from 'react';
+import { StyleSheet, Slider } from 'react-native';
+import { Text, View } from '../components/Themed';
+import { ColorPicker, toHsv } from 'react-native-color-picker';
+
+
+export default function TabTwoScreen() {
+
+  return (
+    <View style={styles.container}>
+      
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#7DCAC2',
+  },
+
+});
