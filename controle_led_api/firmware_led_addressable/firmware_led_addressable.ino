bdd834b8986979b0eec4ce341e94edda6f549875
mest2011 <mest2011mest@gmail.com>
2021-07-31 13:42:38 -0300

    Rename directories of firmwares and create firmware of led addressable

---
+++ controle_led_api/firmware_led_addressable/firmware_led_addressable.ino
@@ -0,0 +1,275 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266HTTPClient.h>
+#include <ArduinoJson.h>
+#include <FastLED.h>
+
+const char *ssid = "mest tp";
+const char *password = "19800524";
+
+#define DATA_PIN    13        //pino do arduino conectado a fita de LEDS WS2812B
+#define LED_TYPE    WS2812B
+#define COLOR_ORDER GRB     //Ordem das cores na fita de led WS2812B
+#define NUM_LEDS    146  
+
+CRGB leds[NUM_LEDS];
+
+#define BRIGHTNESS          200   //brilho dos leds  0 a 255
+#define FRAMES_PER_SECOND  100   //frames por segundo, quanto mais leds na fita menor deve ser a taxa de frames por segundo
+
+int r = 0;
+int g = 0;
+int b = 0;
+
+double intensidade = 1.0;
+
+
+//Configuracao da API
+const char *nameDevice = "rgb";
+char url[255];
+const char *url_p1 = "http://api.mesttech.com.br/iot/?name=";
+const char *url_p2 = "&status=status";
+
+const char *url_api_laravel = "http://mesttech.com.br/api/iot_api/public/api/state/id1";
+
+//Outras variaveis
+int contadorDeErros = 0;
+
+void setup()
+{
+  delay(3000); // 3 second delay for recovery
+  FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(BRIGHTNESS);
+
+  Serial.begin(115200);
+  Serial.write(12);
+  WiFi.begin(ssid, password);
+
+  doConcat(url_p1, nameDevice, url);
+  doConcat(url, url_p2, url); 
+  
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    error("Connectando ao WIFI...", 0);
+  }
+}
+
+void loop()
+{
+
+  if (WiFi.status() == WL_CONNECTED)
+  {
+    HTTPClient http; //Object of class HTTPClient
+    http.begin(url_api_laravel);
+    int httpCode = http.GET();
+
+    if (httpCode > 0)
+    {
+      contadorDeErros = 0;
+      
+      const size_t bufferSize = JSON_OBJECT_SIZE(2) + JSON_OBJECT_SIZE(3) + JSON_OBJECT_SIZE(5) + JSON_OBJECT_SIZE(8) + 370;
+      DynamicJsonBuffer jsonBuffer(bufferSize);
+      JsonObject &root = jsonBuffer.parseObject(http.getString());
+
+      int id = (int)root["id_lampada"];
+      int state = (int)root["status"];
+      const char *name = root["nome"];
+      int tempo = (int)root["tempo"];
+      const char *funcao = root["funcao"];
+      const char *cor = root["cor"];
+      intensidade = (double)root["intensidade"] / 100;
+      r = (int)root["cores"]["r"];
+      g = (int)root["cores"]["g"];
+      b = (int)root["cores"]["b"];
+
+      Serial.println("Resposta da Api: \n");
+      Serial.print("Nome da lampada: ");
+      Serial.println(name);
+      Serial.print("Estado da lampada: ");
+      Serial.println(state);
+      Serial.print("Tempo: ");
+      Serial.println(tempo);
+      Serial.print("Funcao: ");
+      Serial.println(funcao);
+      Serial.print("Intensidade: ");
+      Serial.println(intensidade * 100);
+      Serial.print("Cor em HEX: ");
+      Serial.println(cor);
+      Serial.println("Cor em RGB: ");
+      Serial.print("Red: ");
+      Serial.println(r);
+      Serial.print("Green: ");
+      Serial.println(g);
+      Serial.print("Blue: ");
+      Serial.println(b);
+
+      //setIntensidade();
+
+      if (state == 0)
+      {
+        changeState(state);
+        delay(3000);
+      }
+      else
+      {
+        // if ((String)funcao == "fade")
+        // {
+        //   fadeInOut(tempo);
+        // }
+        // else if ((String)funcao == "blink")
+        // {
+        //   stroble(tempo);
+        // }
+        // else if ((String)funcao == "carrossel")
+        // {
+        //   carrossel(tempo);
+        // }else{
+          setColor(r, g, b);
+          delay(3000);
+        //}
+      }
+    }
+    else
+    {
+      if(contadorDeErros >= 3){
+        error("Erro na requisição dos dados da API!", 1);
+      }else{
+        contadorDeErros++;
+        delay(2000);
+      }
+    }
+    http.end(); //Close connection
+  }
+  else
+  {
+    error("Connectando ao WIFI...", 0);
+  }
+}
+
+void changeState(int state)
+{
+  if (state == 1)
+  {
+    setColor(r, g, b);
+  }
+  else
+  {
+    setColor(0, 0, 0);
+  } 
+}
+
+void flame()
+{
+}
+
+void setColor(int r, int g, int b)
+{  
+  for(int x=0;x < NUM_LEDS; x++){
+    leds[x] = CRGB(r, g, b);
+  }
+  FastLED.show();
+}
+
+// void setIntensidade(){
+//   r = r * intensidade;
+//   g = g * intensidade;
+//   b = b * intensidade;
+// }
+
+//Funcoes internas
+void doConcat(const char *a, const char *b, char *out) {
+    strcpy(out, a);
+    strcat(out, b);
+}
+
+//------------Efeitos-------------
+
+void error(String msg, int type)
+{
+  if(type == 0){
+    r = 1023;
+    g = 0;
+    b = 0;
+  }
+  if(type == 1){
+    r = 920;
+    g = 644;
+    b = 0;
+  }
+  
+  Serial.println(msg);
+  fadeInOut(10);
+}
+
+void stroble(int intervalo)
+{
+  for (int i = 0; i < 100; i++)
+  {
+    changeState(1);
+    delay(intervalo);
+    changeState(0);
+    delay(intervalo);
+  }
+}
+
+void fadeInOut(int tempo)
+{
+  for(int execution_fade = 0;execution_fade < 1; execution_fade++){
+    for (double i = 1.0; i > 0; i=i-0.001)
+    {
+      setColor((r*i), (g*i), (b*i));
+      delay(tempo / 10);
+    }
+    
+    for (double i = 0.0; i < 1; i=i+0.001)
+    {
+      setColor((r*i), (g*i), (b*i));
+      delay(tempo / 10);
+    }
+  }
+  
+  
+}
+
+void carrossel(int tempo)
+{
+  tempo = tempo/5;
+  int rtemp = 0;
+  int gtemp = 0;
+  int btemp = 1023;
+  //sobe verde
+  for (gtemp = 0; gtemp < 1023; gtemp = gtemp + 5)
+  {
+    setColor((rtemp * intensidade), (gtemp * intensidade), (btemp * intensidade));
+    delay(tempo);
+  }
+  //desce azul
+  for (btemp = 1023; btemp > 0; btemp = btemp - 5)
+  {
+    setColor((rtemp * intensidade), (gtemp * intensidade), (btemp * intensidade));
+    delay(tempo);
+  }
+  //sobe vermelho
+  for (rtemp = 0; rtemp < 1023; rtemp = rtemp + 5)
+  {
+    setColor((rtemp * intensidade), (gtemp * intensidade), (btemp * intensidade));
+    delay(tempo);
+  }
+  //desce verde
+  for (gtemp = 1023; gtemp > 0; gtemp = gtemp - 5)
+  {
+    setColor((rtemp * intensidade), (gtemp * intensidade), (btemp * intensidade));
+    delay(tempo);
+  }
+  //sobe azul
+  for (btemp = 0; btemp < 1023; btemp = btemp + 5)
+  {
+    setColor((rtemp * intensidade), (gtemp * intensidade), (btemp * intensidade));
+    delay(tempo);
+  }
+  //desce vermelho
+  for (rtemp = 1023; rtemp > 0; rtemp = rtemp - 5)
+  {
+    setColor((rtemp * intensidade), (gtemp * intensidade), (btemp * intensidade));
+    delay(tempo);
+  }
+}
